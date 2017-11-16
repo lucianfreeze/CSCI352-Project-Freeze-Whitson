3d1a6fd1a42bdacd11760d38a8afebe588a3f982
Lucian Freeze <lucpfree@ut.utm.edu>
2017-11-16 14:29:27 -0600

    Added Account classes.

---
+++ Interface Mockups/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class BankAccount
+{
+    private float Balance;
+    private string AccountName;
+
+	public BankAccount()
+	{
+
+	}
+}
+
+public class CheckingAccount : BankAccount
+{
+
+}
+
+public class SavingsAccount : BankAccount
+{
+
+}
---
+++ Interface Mockups/UserAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class UserAccount
+{
+    private string Email;
+    private string Password;
+    private string FirstName;
+    private string LastName;
+    private List<BankAccount> BankAccounts;
+
+    public UserAccount(string Email, string Password, string FirstName, string LastName)
+	{
+        this.Email = Email;
+        this.Password = Password;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+	}
+    
+    public createAccount()
+    {
+
+    }
+}
