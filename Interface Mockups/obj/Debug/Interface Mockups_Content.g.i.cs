b8109b41b3a7661f95b3278f96b42031885f0f6a
Brett Whitson <bremwhit@ut.utm.edu>
2017-11-21 14:16:51 -0600

    added database

--- Interface Mockups/obj/Debug/Interface Mockups_Content.g.i.cs
+++ Interface Mockups/obj/Debug/Interface Mockups_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/feas2.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("database1.accdb")]
 
 
