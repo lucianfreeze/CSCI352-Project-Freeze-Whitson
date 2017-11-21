cc2b888b79392ed17830f36f7af87e70a9305e23
Brett Whitson <bremwhit@ut.utm.edu>
2017-11-21 13:46:21 -0600

    take main

---
+++ Interface Mockups/obj/Debug/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace Interface_Mockups {
+
+    public partial class ReclaimMain : System.Windows.Application
+    {
+        public void InitializeComponent()
+        {
+            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+        }
+
+        [System.STAThreadAttribute()]
+        public static void main()
+        {
+            Interface_Mockups.ReclaimMain app = new Interface_Mockups.ReclaimMain();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
