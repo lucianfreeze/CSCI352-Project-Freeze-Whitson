51d50eb34d6cf86a76dff88cb5b277662b6dd995
Brett Whitson <bremwhit@ut.utm.edu>
2017-11-21 13:47:39 -0600

    added more stuff in main

---
+++ Interface Mockups/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace Interface_Mockups {
+
+    public partial class ReclaimMain : System.Windows.Application
+    {
+        public void InitializeComponent()
+        {
+            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+        }
+
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            Interface_Mockups.ReclaimMain app = new Interface_Mockups.ReclaimMain();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
