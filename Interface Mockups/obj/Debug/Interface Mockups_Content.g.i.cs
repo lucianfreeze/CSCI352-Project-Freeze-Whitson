e84ea2393c000d06afb29886a1c4001a6ecad708
Lucian Freeze <lucpfree@ut.utm.edu>
2017-12-05 00:04:39 -0600

    Updating some XAML components

--- Interface Mockups/obj/Debug/Interface Mockups_Content.g.i.cs
+++ Interface Mockups/obj/Debug/Interface Mockups_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("database1.accdb")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("notes.txt")]
 
 
